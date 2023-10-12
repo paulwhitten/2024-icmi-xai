c142e1eefd44888052cced1f64887c0b09a680e6
pcw <pcw@case.edu>
2023-10-11 20:45:25 -0400

    corner sample image

--- corner_example.py
+++ corner_example.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import math
 import skimage
+import numpy as np
 from matplotlib import pyplot as plt
 from skimage.feature import corner_harris, corner_subpix, corner_peaks
 from skimage.transform import warp, AffineTransform
@@ -8,9 +10,27 @@ from skimage.draw import ellipse
 
 # from example https://scikit-image.org/docs/stable/auto_examples/features_detection/plot_corner.html#sphx-glr-auto-examples-features-detection-plot-corner-py
 
-# Sheared checkerboard
-tform = AffineTransform(scale=(1.3, 1.1), rotation=1, shear=0.7,
-                        translation=(110, 30))
+# this function returns true if x, y is valid in the image
+def valid_pixel(img, x, y):
+    max_y = img.shape[0]
+    max_x = img.shape[1]
+    if (x >= 0 and x < max_x and y >= 0 and y < max_y):
+        return True
+    else:
+        return False
+
+
+# this function sets the neighborhood of the passed pixel in the image a distance of pixels_away
+def set_neighborhood(img, x, y, pixels_away, intensity):
+    for yi in range(y-pixels_away, y+pixels_away+1):
+        for xi in range(x-pixels_away, x+pixels_away+1):
+            if (valid_pixel(img, xi, yi)):
+                dist = math.sqrt(abs(x-xi) + abs(y-yi))
+                pixel_intensity = int(intensity / (dist + 1))
+                if (img[yi, xi] < pixel_intensity):
+                    img[yi, xi] = pixel_intensity
+
+
 # load image
 print("opening file:", sys.argv[1])
 filename = os.path.join("data", sys.argv[1])
@@ -28,5 +48,13 @@ ax.plot(coords[:, 1], coords[:, 0], color='cyan', marker='o',
         linestyle='None', markersize=6)
 ax.plot(coords_subpix[:, 1], coords_subpix[:, 0], '+r', markersize=10)
 ax.axis((0, 30, 30, 0))
+
+new_image = np.zeros((image.shape[0], image.shape[1]), np.uint8)
+for coord in coords:
+     set_neighborhood(new_image, coord[1], coord[0], 2, 255)
+skimage.io.imsave("test.png", new_image, check_contrast=False)
+
 plt.show()
+
+
 
